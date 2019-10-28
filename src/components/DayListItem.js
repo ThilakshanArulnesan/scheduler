b85d25f9376e29c5bdb1d670e5ee8f6cc3002a75
Thilakshan <th_thilakshan@hotmail.com>
2019-10-28 12:55:10 -0400

    modify label for spots to match design

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -14,10 +14,16 @@ export default function DayListItem(props) {
     'day-list__item--full': props.spot === 0,
   });
 
+  const format = (spots) => {
+    if (spots === 0) return `no spots remaining`;
+    if (spots === 1) return `1 spot remaining`;
+    return `${spots} spots remaining`;
+  }
+
   return (
     <li className={dayClass} onClick={(event) => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{format(props.spots)}</h3>
     </li>
   );
 }
