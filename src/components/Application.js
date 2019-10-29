e8a92874f8d72d282d77ef675e4affe48950ea15
Thilakshan <th_thilakshan@hotmail.com>
2019-10-29 13:18:54 -0400

    remove hardcoded days, make api request instead

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import DayList from "components/DayList";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
 
 
 const appointments = [
@@ -73,6 +56,12 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/days`)
+      .then(res => setDays(res.data));
+  }, [])
 
   return (
     <main className="layout">
